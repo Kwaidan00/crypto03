63f4140dea8a2fb460afecf7c7d4c3fa2f724eb6
Aleksander Spyra <aleksander.spyra@gmail.com>
2015-11-08 18:54:35 +0100

    third commit with src ex1 and ex2

--- src/zad1/my_keystore_f.h
+++ src/zad1/my_keystore_f.h
@@ -174,3 +174,103 @@ int get_decrypted_key(string user_id, string key_id, string keystore, unsigned c
 	keystore_file.close();
 	return -1;
 }
+
+int get_decrypted_key_with_password(string user_id, string key_id, string keystore, unsigned char* decryptedtext, string password)
+{
+	string user_id_key_id_s = user_id + key_id;
+	const char *user_id_key_id_const_char = user_id_key_id_s.c_str();
+	unsigned char user_id_key_id_uchar[user_id_key_id_s.length()];
+	for (int i = 0; i < user_id_key_id_s.length(); i++)
+	{
+		user_id_key_id_uchar[i] = user_id_key_id_const_char[i];
+	}	
+	unsigned char user_id_key_id_hash[EVP_MAX_MD_SIZE];
+	unsigned int user_id_key_id_hash_len = hash_sha512(user_id_key_id_uchar, user_id_key_id_hash, strlen(user_id_key_id_const_char));
+
+	string hashed_ids = hex_table(user_id_key_id_hash, user_id_key_id_hash_len);
+	
+	ifstream keystore_file(keystore);
+	if (keystore_file.is_open() == false)
+	{
+		printf("keystore file not found...\n");
+		return -1;
+	}
+	int nr_of_line = 0;
+	string line = "";
+	while ( getline(keystore_file, line) )
+	{
+		if (hashed_ids.compare(line.substr(0, 128)) == 0)
+		{
+			//Odnaleziono zgodne hashe
+
+			// Wczytanie hasla
+		// Hashowanie password
+			const char *password_const_char = password.c_str();
+			unsigned char password_uchar[password.length()];
+			for (int i = 0; i < password.length(); i++)
+			{
+				password_uchar[i] = password_const_char[i];
+			}	
+			unsigned char password_hash[EVP_MAX_MD_SIZE];
+			unsigned int password_hash_len = hash_sha512(password_uchar, password_hash, strlen(password_const_char));
+			string password_hex = hex_table(password_hash, password_hash_len);
+			if ( password_hex.compare(line.substr(128, 128)) == 0 )
+			{
+				//printf("ACCESS GRANTED\n");
+			}
+			else
+			{
+				printf("ACCES TO KEYSTORE DENIED\n");
+				return -1;
+			}
+		// Wyciagnij ostatnie rzeczy z linii, odhexuj je - jest zaszyfrowany klucz. Utworzenie klucza i VI i odszyfrowanie.
+			string encrypted_key = unhex_table( line.substr(256, 96) );
+			unsigned char encrypted_key_u[encrypted_key.length()];
+			for (int i = 0; i < encrypted_key.length(); i++)
+			{
+				encrypted_key_u[i] = encrypted_key[i];
+			}
+		//---------------------------------------------
+			ostringstream nr_of_lines_ss;
+			nr_of_lines_ss << nr_of_line;
+			string nr_of_lines_string = nr_of_lines_ss.str();
+
+			unsigned char* key0;
+			key0 = (unsigned char*)malloc(32*sizeof(unsigned char));
+			string pass_nr = password + nr_of_lines_string;
+			seed_seq seed1 (pass_nr.begin(),pass_nr.end());
+			mt19937 mersenne1(seed1);
+			for (int i = 0; i < 32; i++) 
+			{
+				key0[i] = (unsigned char)mersenne1();
+			}
+
+			unsigned char* iv0;
+			iv0 = (unsigned char*)malloc(16*sizeof(unsigned char));
+			string nr_pass = nr_of_lines_string + password;
+			seed_seq seed2 (pass_nr.begin(),pass_nr.end());
+			mt19937 mersenne2(seed2);
+			for (int i = 0; i < 16; i++)
+			{
+				iv0[i] = (unsigned char)mersenne2();
+			}
+
+			int decryptedtext_len;
+			ERR_load_crypto_strings();
+			OpenSSL_add_all_algorithms();
+			OPENSSL_config(NULL);
+
+			decryptedtext_len = decrypt_aes_256_cbc( encrypted_key_u, encrypted_key.length(), key0, iv0, decryptedtext);
+
+
+			EVP_cleanup();
+			ERR_free_strings();
+	
+			keystore_file.close();
+			return decryptedtext_len;
+		}
+		nr_of_line += 1;
+	}
+	keystore_file.close();
+	return -1;
+}
